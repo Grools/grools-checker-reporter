4c892f439ddc2fab8c276361df3eddae4adcae49
bioinfornatics <bioinfornatics@fedoraproject.org>
2016-11-28 10:12:07 +0100

    check undefined element

--- src/main/resources/js/svg_common.js
+++ src/main/resources/js/svg_common.js
@@ -18,24 +18,31 @@ Array.prototype.hasObject = (
 );
 
 function hasClass(el, className) {
-  if (el.classList)
-    return el.classList.contains(className)
+  if( typeof el == 'undefined' )
+    return false;
+  else if (el.classList)
+    return el.classList.contains(className);
   else
-    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
 }
 
 function addClass(el, className) {
-  if (el.classList)
-    el.classList.add(className)
-  else if (!hasClass(el, className)) el.className += ' ' + className
+  if( typeof el == 'undefined' ){
+      if (el.classList)
+        el.classList.add(className);
+      else if (!hasClass(el, className))
+        el.className += ' ' + className;
+  }
 }
 
 function removeClass(el, className) {
-  if (el.classList)
-    el.classList.remove(className)
-  else if (hasClass(el, className)) {
-    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-    el.className=el.className.replace(reg, ' ')
+  if( typeof el == 'undefined' ){
+      if (el.classList)
+        el.classList.remove(className);
+      else if (hasClass(el, className)) {
+        var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+        el.className=el.className.replace(reg, ' ');
+      }
   }
 }
 
